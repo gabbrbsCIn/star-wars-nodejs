082ad748410d625bd0d303deeb9a6c1eb6cdc786
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-10-14 15:34:55 -0300

    feat: add rotas base da tabela de personagens

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import authRoutes from "../src/routes/auth/authRoutes";
+import characterRoutes from "./routes/character/characterRoutes"
 require("dotenv").config();
 
 const app = express();
@@ -7,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 
-app.use("/", authRoutes);
+app.use("/characters", characterRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}...`);
